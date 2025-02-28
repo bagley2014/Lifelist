ae82968a734813b797dd1e08dd0a1f9ee55361fc
bagley2014 <dbagley@harding.edu>
2025-02-27 20:01:19 -0800

    Initial Commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  env: {
+    es2017: true,
+    node: true,
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};
