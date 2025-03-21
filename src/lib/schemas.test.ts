cfebc329396268d1c16f94fbba7643c64ad9ea77
bagley2014 <dbagley@harding.edu>
2025-03-21 13:37:31 -0700

    WIP - Improve date parsing code
    We check if a time is midnight and treat it as if it doesn't exist if so. This approach ran into timezone issues.
    
    There's another timezone issue where passing dates between the client and the server results in an off by one error where the first date of the results the client receives is yesterday instead of today.
    
    This commit starts using a more robust library to hopefully eliminate timezone issues as a source of bugs moving forward.
    
    We also add Jest in order to test some of the datetime parsing code.

--- src/lib/schemas.test.ts
+++ src/lib/schemas.test.ts
@@ -1,27 +1,43 @@
+import { Frequency, dateTimeSchema, eventSchema } from './schemas';
 import { describe, expect, test } from '@jest/globals';
 
-import { myTempDateSchema } from './schemas';
+import { DateTime } from 'luxon';
 
-describe('myDateSchema', () => {
+describe('dateTimeSchema', () => {
+	test("doesn't modify existing DateTime objects", () => {
+		const date = new Date();
+		const dateTime = dateTimeSchema.required().validateSync(DateTime.fromJSDate(date));
+		expect(dateTime).toBeInstanceOf(DateTime);
+		expect(dateTime.isValid).toBe(true);
+		expect(dateTime.toJSDate()).toEqual(date);
+		expect(dateTime).toStrictEqual(DateTime.fromJSDate(date));
+	});
+
 	describe('parses dates', () => {
 		test('with the year first', () => {
-			const result = myTempDateSchema.required().validateSync('1996-01-25');
+			const result = dateTimeSchema.required().validateSync('1996-01-25');
 			expect(result.isValid).toBe(true);
-			expect(result?.day).toEqual(25);
-			expect(result?.month).toEqual(1);
-			expect(result?.year).toEqual(1996);
+			expect(result.day).toEqual(25);
+			expect(result.month).toEqual(1);
+			expect(result.year).toEqual(1996);
+			expect(result.hour).toEqual(0);
+			expect(result.minute).toEqual(0);
+			expect(result.second).toEqual(0);
 		});
 		test('in the American order', () => {
-			const result = myTempDateSchema.required().validateSync('6/13/2014');
+			const result = dateTimeSchema.required().validateSync('6/13/2014');
 			expect(result.isValid).toBe(true);
 			expect(result.day).toEqual(13);
 			expect(result.month).toEqual(6);
 			expect(result.year).toEqual(2014);
+			expect(result.hour).toEqual(0);
+			expect(result.minute).toEqual(0);
+			expect(result.second).toEqual(0);
 		});
 	});
 	describe('parses times', () => {
 		test('with an informal format', () => {
-			const result = myTempDateSchema.required().validateSync('7pm PT');
+			const result = dateTimeSchema.required().validateSync('7pm PT');
 			expect(result.isValid).toBe(true);
 			expect(result.hour).toEqual(19);
 			expect(result.minute).toEqual(0);
@@ -32,7 +48,7 @@ describe('myDateSchema', () => {
 			expect(timezone.offsetName(1742459157854, { format: 'short', locale: 'en-US' })).toEqual('PDT');
 		});
 		test('with a formal format', () => {
-			const result = myTempDateSchema.required().validateSync('14:30:00 CT');
+			const result = dateTimeSchema.required().validateSync('14:30:00 CT');
 			expect(result.isValid).toBe(true);
 			expect(result.hour).toEqual(14);
 			expect(result.minute).toEqual(30);
@@ -43,7 +59,7 @@ describe('myDateSchema', () => {
 			expect(timezone.offsetName(1742459157854, { format: 'short', locale: 'en-US' })).toEqual('CDT');
 		});
 		test('with a foreign timezone', () => {
-			const result = myTempDateSchema.required().validateSync('5:00 AM JST');
+			const result = dateTimeSchema.required().validateSync('5:00 AM JST');
 			expect(result.isValid).toBe(true);
 			expect(result.hour).toEqual(5);
 			expect(result.minute).toEqual(0);
@@ -51,6 +67,87 @@ describe('myDateSchema', () => {
 
 			const timezone = result.zone;
 			expect(timezone.offsetName(1740043708614, { format: 'short', locale: 'en-US' })).toEqual('UTC+9');
+		});
+	});
+
+	describe('fails input', () => {
+		test('that is very vague', () => {
+			expect(() => {
+				dateTimeSchema.required().validateSync('12');
+			}).toThrow();
+		});
+	});
+});
+
+describe('eventSchema', () => {
+	const validEvent = {
+		name: 'Test Event',
+		priority: 5,
+		start: '2022-01-01',
+		end: '2022-01-02',
+		location: 'Test Location',
+		tags: ['Test Tag'],
+	};
+
+	describe('to pass', () => {
+		test('on a valid event', () => {
+			const result = eventSchema.validateSync(validEvent);
+			expect(result).toEqual({
+				...validEvent,
+				frequency: Frequency.Once,
+				start: dateTimeSchema.validateSync(validEvent.start),
+				end: dateTimeSchema.validateSync(validEvent.end),
+			});
+		});
+	});
+
+	describe('to fail', () => {
+		test('on an event with missing required fields', () => {
+			expect(() => {
+				eventSchema.validateSync({});
+			}).toThrow();
+		});
+
+		test('on an event with an invalid name', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, name: '' });
+			}).toThrow();
+		});
+
+		test('on an event with an invalid priority', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, priority: -1 });
+			}).toThrow();
+		});
+
+		test('on an event with an invalid frequency', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, frequency: 'invalid' });
+			}).toThrow();
+		});
+
+		test('on an event with an invalid start date', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, start: 'invalid' });
+			}).toThrow();
+		});
+
+		test('on an event with an invalid end date', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, end: 'invalid' });
+			}).toThrow();
+		});
+
+		test('on an event with an end date before the start date', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, end: '2021-01-01' });
+			}).toThrow();
+		});
+
+		test('on an event with an end date but no start date', () => {
+			expect(() => {
+				eventSchema.validateSync({ ...validEvent, start: null });
+			}).toThrow();
 		});
 	});
 });
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -1,7 +1,7 @@
 import * as chrono from 'chrono-node';
 
 import { DateTime, FixedOffsetZone, IANAZone } from 'luxon';
-import { InferType, array, date, mixed, number, object, ref, string } from 'yup';
+import { InferType, array, mixed, number, object, string } from 'yup';
 
 import { extractAndConvertTimezone } from './timezones';
 
@@ -20,11 +20,6 @@ chrono.strict.refiners.push({
 				result.start.assign('hour', 0);
 				result.start.assign('minute', 0);
 				result.start.assign('second', 0);
-			} else if (!result.start.isCertain('minute')) {
-				result.start.assign('minute', 0);
-				result.start.assign('second', 0);
-			} else if (!result.start.isCertain('second')) {
-				result.start.assign('second', 0);
 			}
 		});
 		return results;
@@ -42,58 +37,49 @@ chrono.strict.refiners.push({
 // .nonNullable -> remove null
 // .required -> remove undefined and null
 
-const myDateSchema = date().transform((value, originalValue, context) => {
-	if (value === null || value === undefined) return value;
+export const dateTimeSchema = mixed<DateTime>((input): input is DateTime => input instanceof DateTime)
+	.transform((value, originalValue, context) => {
+		// We can't do anything with null or undefined
+		if (value === null || value === undefined) return value;
 
-	if (context.isType(value)) return value;
+		// If the value is already a DateTime object, then we don't need to do anything
+		if (context.isType(value)) return value;
 
-	return chrono.strict.parseDate(originalValue);
-});
+		// Otherwise, we need to parse the value
+		const parseResults = chrono.strict.parse(originalValue);
 
-export const myTempDateSchema = mixed<DateTime>((input): input is DateTime => input instanceof DateTime).transform((value, originalValue, context) => {
-	// We can't do anything with null or undefined
-	if (value === null || value === undefined) return value;
+		// If the value is not a valid date, then we return null
+		if (parseResults.length === 0) return null;
 
-	// If the value is already a DateTime object, then we don't need to do anything
-	if (context.isType(value)) return value;
+		// If the value is a valid date, then we return a new DateTime object
+		const year = parseResults[0].start.isCertain('year') ? parseResults[0].start.get('year')! : undefined;
+		const month = parseResults[0].start.isCertain('month') ? parseResults[0].start.get('month')! : undefined;
+		const day = parseResults[0].start.isCertain('day') ? parseResults[0].start.get('day')! : undefined;
+		const hour = parseResults[0].start.get('hour')!;
+		const minute = parseResults[0].start.get('minute')!;
+		const timezoneOffset = parseResults[0].start.isCertain('timezoneOffset') ? parseResults[0].start.get('timezoneOffset')! : undefined;
+		const ianaTimezone = extractAndConvertTimezone(originalValue);
+		return DateTime.fromObject(
+			{
+				year: year,
+				month: month,
+				day: day,
+				hour: hour,
+				minute: minute,
+			},
+			ianaTimezone ? { zone: IANAZone.create(ianaTimezone) } : timezoneOffset ? { zone: FixedOffsetZone.instance(timezoneOffset) } : {},
+		);
+	})
+	.test('is-valid', '${path} is not a valid date', value => value?.isValid);
 
-	// Otherwise, we need to parse the value
-	const parseResults = chrono.strict.parse(originalValue);
-
-	// If the value is not a valid date, then we return null
-	if (parseResults.length === 0) return null;
-
-	// If the value is a valid date, then we return a new DateTime object
-	const year = parseResults[0].start.isCertain('year') ? parseResults[0].start.get('year')! : undefined;
-	const month = parseResults[0].start.isCertain('month') ? parseResults[0].start.get('month')! : undefined;
-	const day = parseResults[0].start.isCertain('day') ? parseResults[0].start.get('day')! : undefined;
-	const hour = parseResults[0].start.isCertain('hour') ? parseResults[0].start.get('hour')! : undefined;
-	const minute = parseResults[0].start.isCertain('minute') ? parseResults[0].start.get('minute')! : undefined;
-	const second = parseResults[0].start.isCertain('second') ? parseResults[0].start.get('second')! : undefined;
-	const timezoneOffset = parseResults[0].start.isCertain('timezoneOffset') ? parseResults[0].start.get('timezoneOffset')! : undefined;
-	const ianaTimezone = extractAndConvertTimezone(originalValue);
-	return DateTime.fromObject(
-		{
-			year: year,
-			month: month,
-			day: day,
-			hour: hour,
-			minute: minute,
-			second: second,
-		},
-		ianaTimezone ? { zone: IANAZone.create(ianaTimezone) } : timezoneOffset ? { zone: FixedOffsetZone.instance(timezoneOffset) } : {},
-	);
-});
-
-const eventSchema = object({
+export const eventSchema = object({
 	name: string().required(),
 	priority: number().required().min(0).max(10),
 	location: string().notRequired().default(null),
-	start: myDateSchema.defined().nullable().default(null),
-	end: myDateSchema.notRequired().when('start', {
+	start: dateTimeSchema.defined().nullable().default(null),
+	end: dateTimeSchema.notRequired().when('start', {
 		is: null,
 		then: schema => schema.test('is-null-or-undefined', '${path} is defined but start is not', value => value === null || value === undefined),
-		otherwise: schema => schema.min(ref('start'), 'End date must be after start date'),
 	}),
 	frequency: mixed<Frequency>()
 		.oneOf(Object.values(Frequency))
@@ -103,9 +89,15 @@ const eventSchema = object({
 			then: schema => schema.test('is-once', '${path} must be "once" if start is null', value => value == 'once'),
 		}),
 	tags: array().of(string().required()).default([]),
-}).exact();
+})
+	.exact()
+	.test('start-before-end', 'start must be before end', function (value) {
+		if (value.start && value.end && value.start >= value.end) {
+			return this.createError({ path: 'start' });
+		}
+		return true;
+	});
 
 export const dataSchema = object({ upcoming: array().of(eventSchema).required() }).exact();
 
 export type Event = InferType<typeof eventSchema>;
-export type MyDate = InferType<typeof myDateSchema>;
