bagley2014/Lifelist

commit 74ef031f3d8ed55777b833748a9a35c6049843fd

WIP - Improve date parsing code
This commit continues the migration to luxon in an attempt to eradicate timezone issues.

The events enumeration logic is refactored to use the new library, as well as in preparation to test the code. However, jest doesn't play nicely with importing the yaml package.

In the process this also introduces a bug where my biweekly events get duplicated and end up with on the wrong day...

src/lib/events.ts
@@ -1,32 +1,16 @@
 import { Event, Frequency, dataSchema } from './schemas';
 
+import { DateTime } from 'luxon';
 import { debounce } from './util';
 import { promises as fsp } from 'fs';
 import { inPlaceSort } from 'fast-sort';
 import { watch } from 'node:fs';
 import { parse as yamlParse } from 'yaml';
 
-// GLOBALS ******
 const DATA_FILE = process.env.DATA_FILE ?? 'example/data.yaml';
-console.log(`Using data file: ${DATA_FILE}`);
-
+let INITIALIZED = false;
 let CACHED_RESULTS: { [key: string]: [Event[], AsyncGenerator<Event, void, unknown>] } = {};
-
-// TODO: Make this first call lazy
-let PARSED_EVENTS = parseEvents();
-
-const debouncedParse = debounce(() => {
-	CACHED_RESULTS = {};
-	PARSED_EVENTS = parseEvents();
-}, 500);
-
-// When the data file changes, we parse it again and clear the old cache
-watch(DATA_FILE, eventType => {
-	console.log(`Data file changed (${eventType})`);
-	if (eventType === 'change') debouncedParse();
-});
-
-// **************
+let PARSED_EVENTS: Promise<Event[]>;
 
 export interface EventSummary {
 	name: string;
@@ -37,10 +21,26 @@ export interface EventSummary {
 	tags: string[];
 }
 
-function addDays(date: Date, days: number) {
-	const newDate = new Date(date);
-	newDate.setDate(newDate.getDate() + days);
-	return newDate;
+function init() {
+	if (INITIALIZED) throw new Error('Already initialized - `init` should only be called once');
+
+	console.log(`Using data file: ${DATA_FILE}`);
+
+	// TODO: Make this first call lazy
+	PARSED_EVENTS = parseEvents();
+
+	const debouncedParse = debounce(() => {
+		CACHED_RESULTS = {};
+		PARSED_EVENTS = parseEvents();
+	}, 500);
+
+	// When the data file changes, we parse it again and clear the old cache
+	watch(DATA_FILE, eventType => {
+		console.log(`Data file changed (${eventType})`);
+		if (eventType === 'change') debouncedParse();
+	});
+
+	INITIALIZED = true;
 }
 
 function sortEvents(events: Event[]) {
@@ -54,14 +54,14 @@ async function parseEvents() {
 		encoding: 'utf8',
 	});
 
-	const events = dataSchema.cast(yamlParse(eventDataFileContent)).upcoming;
+	const events = dataSchema.validateSync(yamlParse(eventDataFileContent)).upcoming;
 	sortEvents(events);
 
 	console.log('Events parsed');
 	return events;
 }
 
-async function* enumerateEventsFromDate(date: Date = new Date()) {
+async function* enumerateEventsFromDate(date: DateTime) {
 	// Clone the parsed events data so we can modify it
 	const events = (await PARSED_EVENTS).slice();
 
@@ -72,18 +72,18 @@ async function* enumerateEventsFromDate(date: Date = new Date()) {
 		if (event.frequency !== Frequency.Once) {
 			// The schema ensures that any event with a frequency other than "once" has a start date
 			if (event.frequency === Frequency.Weekly) {
-				const nextEvent = { ...event, start: addDays(event.start!, 7), end: event.end ? addDays(event.end, 7) : event.end };
+				const nextEvent = { ...event, start: event.start!.plus({ weeks: 1 }), end: event.end ? event.end.plus({ weeks: 1 }) : event.end };
 				events.push(nextEvent);
 			} else if (event.frequency === Frequency.Biweekly) {
-				const nextEvent = { ...event, start: addDays(event.start!, 14), end: event.end ? addDays(event.end, 14) : event.end };
+				const nextEvent = { ...event, start: event.start!.plus({ weeks: 2 }), end: event.end ? event.end.plus({ weeks: 2 }) : event.end };
 				events.push(nextEvent);
 			} else if (event.frequency === Frequency.Weekdays) {
 				let daysToAdd = 1;
-				let nextDay = addDays(event.start!, daysToAdd);
-				while (nextDay.getDay() === 0 || nextDay.getDay() === 6) {
-					nextDay = addDays(event.start!, ++daysToAdd);
+				let nextDay = event.start!.plus({ days: daysToAdd });
+				while (nextDay.isWeekend) {
+					nextDay = event.start!.plus({ days: ++daysToAdd });
 				}
-				const nextEvent = { ...event, start: nextDay, end: event.end ? addDays(event.end, daysToAdd) : event.end };
+				const nextEvent = { ...event, start: nextDay, end: event.end ? event.end.plus({ days: daysToAdd }) : event.end };
 				events.push(nextEvent);
 			}
 
@@ -97,9 +97,9 @@ async function* enumerateEventsFromDate(date: Date = new Date()) {
 		}
 
 		// Add a new event for events that span multiple days
-		if (event.end && event.end.getDate() > event.start!.getDate()) {
+		if (event.end && event.end > event.start!) {
 			// The schema ensures that any event with an end date has a start date
-			const nextEvent = { ...event, start: addDays(event.start!, 1) };
+			const nextEvent = { ...event, start: event.start!.plus({ days: 1 }) };
 			events.push(nextEvent);
 
 			// Sort the events after adding a new one
@@ -113,9 +113,14 @@ async function* enumerateEventsFromDate(date: Date = new Date()) {
 	}
 }
 
-async function enumerateEvents(from: Date, count: number): Promise<Event[]> {
+function toDateString(date: DateTime) {
+	// The client expects dates in a form like "Wed Mar 19 2025" since that's what `Date.toDateString` outputs
+	return date.toFormat('ccc LLL dd yyyy');
+}
+
+async function enumerateEvents(from: DateTime, count: number): Promise<Event[]> {
 	// The key for the cache is the date without the time
-	const cacheKey = from.toDateString();
+	const cacheKey = toDateString(from);
 
 	// Initialize the cache if it didn't already exist
 	if (!CACHED_RESULTS[cacheKey]) CACHED_RESULTS[cacheKey] = [[], enumerateEventsFromDate(from)];
@@ -135,35 +140,27 @@ async function enumerateEvents(from: Date, count: number): Promise<Event[]> {
 	return CACHED_RESULTS[cacheKey][0];
 }
 
-function groupAndCleanEvents(events: Event[], timezone: string) {
+function groupAndCleanEvents(events: Event[]) {
 	// The client prints the times as is, so formatting is left to the server
-	// But the client expects dates in a form like "Wed Mar 19 2025" since that's what `toDateString` outputs
-	const TIME_FORMAT = new Intl.DateTimeFormat('en-US', {
-		hour: 'numeric',
-		minute: 'numeric',
-		hour12: true,
-		timeZone: timezone,
-		timeZoneName: 'shortGeneric',
-	});
-	const DATE_FORMAT = new Intl.DateTimeFormat('en-US', {
-		timeZone: timezone,
-		weekday: 'short',
-		year: 'numeric',
-		month: 'short',
-		day: 'numeric',
-	});
+	const getTime = (date: DateTime) =>
+		date.toLocaleString({
+			hour: 'numeric',
+			minute: 'numeric',
+			hour12: true,
+			timeZoneName: 'shortGeneric',
+		});
 
 	const groups: { [key: string]: EventSummary[] } = {};
 
 	for (const event of events) {
-		const key = event.start ? DATE_FORMAT.format(event.start).replace(/,/g, '') : 'TODO';
+		const key = event.start ? toDateString(event.start) : 'TODO';
 		if (!groups[key]) groups[key] = [];
 		groups[key].push({
 			name: event.name,
 			priority: event.priority,
 			location: event.location,
-			startTime: event.start && (event.start.getHours() !== 0 || event.start.getMinutes() !== 0) ? TIME_FORMAT.format(event.start!) : undefined,
-			endTime: event.end && (event.end.getHours() !== 0 || event.end.getMinutes() !== 0) ? TIME_FORMAT.format(event.end!) : undefined,
+			startTime: event.start && (event.start.hour !== 0 || event.start.minute !== 0) ? getTime(event.start!) : undefined,
+			endTime: event.end && (event.end.hour !== 0 || event.end.minute !== 0) ? getTime(event.end!) : undefined,
 			tags: event.tags,
 		});
 	}
@@ -176,8 +173,10 @@ function groupAndCleanEvents(events: Event[], timezone: string) {
 	return results;
 }
 
-export async function events(from: Date, count: number, timezone: string): Promise<[string, EventSummary[]][]> {
+export async function events(from: DateTime, count: number): Promise<[string, EventSummary[]][]> {
+	if (!INITIALIZED) init();
+
 	const events = await enumerateEvents(from, count);
-	const groups = groupAndCleanEvents(events, timezone);
+	const groups = groupAndCleanEvents(events);
 	return groups;
 }

src/app/api/events/route.ts
@@ -1,30 +1,29 @@
 import { type NextRequest } from 'next/server';
-import { number, date, ValidationError, string } from 'yup';
+import { number, ValidationError, string } from 'yup';
 import { events } from '@/lib/events';
+import { dateTimeSchema } from '@/lib/schemas';
+import { DateTime } from 'luxon';
 
 const countSchema = number().required('`count` query parameter is required').min(0, '`count` must not be negative');
-const dateSchema = date()
-	.default(new Date())
-	.transform((_, x: string): Date => {
-		const date = new Date(x);
-		return !x || isNaN(date.getTime()) ? new Date() : date;
-	});
 const timezoneSchema = string()
 	.required('`timezone` query parameter is required')
 	.matches(/^[a-zA-Z0-9\/_+-]+$/, '`timezone` must be a valid timezone');
 
 export async function GET(request: NextRequest) {
-	let date,
-		count = 0;
+	let count;
 	let timezone = '';
+	let date;
 	const searchParams = request.nextUrl.searchParams;
 	try {
-		count = countSchema.cast(searchParams.get('count'));
-		countSchema.validateSync(count);
-		date = dateSchema.cast(searchParams.get('date'));
-		dateSchema.validateSync(date);
-		timezone = timezoneSchema.cast(searchParams.get('timezone'));
-		timezoneSchema.validateSync(timezone);
+		count = countSchema.validateSync(searchParams.get('count'));
+		timezone = timezoneSchema.validateSync(searchParams.get('timezone'));
+		date = dateTimeSchema
+			.transform((value, _originalValue, context) => {
+				if (value && context.isType(value)) return value;
+				return DateTime.now().setZone(timezone);
+			})
+			.defined()
+			.validateSync(searchParams.get('date'));
 	} catch (error) {
 		if (error instanceof ValidationError || error instanceof TypeError) return new Response(error.message, { status: 400 });
 		else {
@@ -32,6 +31,7 @@ export async function GET(request: NextRequest) {
 			return new Response('Unknown error', { status: 500 });
 		}
 	}
+
 	// TODO: Add a timeout to keep the client from waiting forever
-	return Response.json(await events(date, count, timezone));
+	return Response.json(await events(date, count));
 }

src/lib/schemas.test.ts
@@ -99,6 +99,16 @@ describe('eventSchema', () => {
 				end: dateTimeSchema.validateSync(validEvent.end),
 			});
 		});
+
+		test('on an event with no end time', () => {
+			const result = eventSchema.validateSync({ ...validEvent, end: undefined });
+			expect(result).toEqual({
+				...validEvent,
+				frequency: Frequency.Once,
+				start: dateTimeSchema.validateSync(validEvent.start),
+				end: undefined,
+			});
+		});
 	});
 
 	describe('to fail', () => {

src/lib/schemas.ts
@@ -48,8 +48,8 @@ export const dateTimeSchema = mixed<DateTime>((input): input is DateTime => inpu
 		// Otherwise, we need to parse the value
 		const parseResults = chrono.strict.parse(originalValue);
 
-		// If the value is not a valid date, then we return null
-		if (parseResults.length === 0) return null;
+		// If the value is not a valid date, then we return an invalid DateTime object
+		if (parseResults.length === 0) return DateTime.invalid('Invalid date');
 
 		// If the value is a valid date, then we return a new DateTime object
 		const year = parseResults[0].start.isCertain('year') ? parseResults[0].start.get('year')! : undefined;
@@ -70,7 +70,7 @@ export const dateTimeSchema = mixed<DateTime>((input): input is DateTime => inpu
 			ianaTimezone ? { zone: IANAZone.create(ianaTimezone) } : timezoneOffset ? { zone: FixedOffsetZone.instance(timezoneOffset) } : {},
 		);
 	})
-	.test('is-valid', '${path} is not a valid date', value => value?.isValid);
+	.test('is-valid', '${path} is not a valid date', value => value === null || value === undefined || value.isValid);
 
 export const eventSchema = object({
 	name: string().required(),
@@ -98,6 +98,6 @@ export const eventSchema = object({
 		return true;
 	});
 
-export const dataSchema = object({ upcoming: array().of(eventSchema).required() }).exact();
+export const dataSchema = object({ upcoming: array().of(eventSchema).required() });
 
 export type Event = InferType<typeof eventSchema>;

commit 92bb196afd18bd04d1d5553e1ffb9a372725be22

Improve events to support testing
And add more tests

Including one to help me find the bug I introduced in 74ef031f

__mocks__/fs.cjs
@@ -1,0 +1,5 @@
+// we can also use `import`, but then
+// every export should be explicitly defined
+
+const { fs } = require('memfs');
+module.exports = fs;

__mocks__/fs/promises.cjs
@@ -1,0 +1,5 @@
+// we can also use `import`, but then
+// every export should be explicitly defined
+
+const { fs } = require('memfs');
+module.exports = fs.promises;

eslint.config.mjs
@@ -15,6 +15,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends('next/core-web-vitals', 'next/typescript'),
+	{ ignores: ['__mocks__/**'] },
 	{
 		plugins: {
 			'@typescript-eslint': typescriptEslint,

src/lib/events.ts
@@ -2,15 +2,14 @@ import { Event, Frequency, dataSchema } from './schemas';
 
 import { DateTime } from 'luxon';
 import { debounce } from './util';
-import { promises as fsp } from 'fs';
+import { promises as fsp, watch, type FSWatcher } from 'fs';
 import { inPlaceSort } from 'fast-sort';
-import { watch } from 'node:fs';
 import { parse as yamlParse } from 'yaml';
 
-const DATA_FILE = process.env.DATA_FILE ?? 'example/data.yaml';
 let INITIALIZED = false;
 let CACHED_RESULTS: { [key: string]: [Event[], AsyncGenerator<Event, void, unknown>] } = {};
 let PARSED_EVENTS: Promise<Event[]>;
+let WATCHER: FSWatcher;
 
 export interface EventSummary {
 	name: string;
@@ -22,6 +21,8 @@ export interface EventSummary {
 }
 
 function init() {
+	const DATA_FILE = process.env.DATA_FILE ?? 'example/data.yaml';
+
 	if (INITIALIZED) throw new Error('Already initialized - `init` should only be called once');
 
 	console.log(`Using data file: ${DATA_FILE}`);
@@ -35,30 +36,44 @@ function init() {
 	}, 500);
 
 	// When the data file changes, we parse it again and clear the old cache
-	watch(DATA_FILE, eventType => {
+	WATCHER = watch(DATA_FILE, eventType => {
 		console.log(`Data file changed (${eventType})`);
 		if (eventType === 'change') debouncedParse();
 	});
 
 	INITIALIZED = true;
+
+	async function parseEvents() {
+		console.log('Parsing events data file');
+		const eventDataFileContent = await fsp.readFile(DATA_FILE, {
+			encoding: 'utf8',
+		});
+
+		const events = dataSchema.validateSync(yamlParse(eventDataFileContent)).upcoming;
+		sortEvents(events);
+
+		console.log('Events parsed');
+		return events;
+	}
+}
+
+export function reset() {
+	if (process.env.NODE_ENV !== 'test') throw new Error('reset should only be called in test environment');
+
+	if (!INITIALIZED) return;
+
+	INITIALIZED = false;
+	CACHED_RESULTS = {};
+	PARSED_EVENTS = Promise.resolve([]);
+	WATCHER.close();
+
+	// console.log was spied on in the test file
+	console.log('Reset');
 }
 
 function sortEvents(events: Event[]) {
 	// Sort the events so that the earliest ones come first (null represents "immediate" TODOs and so should come first)
 	inPlaceSort(events).asc(e => e.start?.valueOf() ?? -1);
-}
-
-async function parseEvents() {
-	console.log('Parsing events data file');
-	const eventDataFileContent = await fsp.readFile(DATA_FILE, {
-		encoding: 'utf8',
-	});
-
-	const events = dataSchema.validateSync(yamlParse(eventDataFileContent)).upcoming;
-	sortEvents(events);
-
-	console.log('Events parsed');
-	return events;
 }
 
 async function* enumerateEventsFromDate(date: DateTime) {
@@ -97,7 +112,7 @@ async function* enumerateEventsFromDate(date: DateTime) {
 		}
 
 		// Add a new event for events that span multiple days
-		if (event.end && event.end > event.start!) {
+		if (event.end && event.end.day > event.start!.day) {
 			// The schema ensures that any event with an end date has a start date
 			const nextEvent = { ...event, start: event.start!.plus({ days: 1 }) };
 			events.push(nextEvent);
@@ -175,6 +190,7 @@ function groupAndCleanEvents(events: Event[]) {
 
 export async function events(from: DateTime, count: number): Promise<[string, EventSummary[]][]> {
 	if (!INITIALIZED) init();
+	else console.log('Already initialized');
 
 	const events = await enumerateEvents(from, count);
 	const groups = groupAndCleanEvents(events);

src/lib/events.test.ts
@@ -1,0 +1,137 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+import { events, reset } from './events';
+
+import { DateTime } from 'luxon';
+import { vol } from 'memfs';
+
+vi.mock('node:fs');
+vi.mock('node:fs/promises');
+
+beforeEach(() => {
+	reset();
+});
+
+afterEach(() => {
+	vol.reset();
+	vi.unstubAllEnvs();
+});
+
+const exampleEventYaml = `
+  - name: "Test Event"
+    priority: 5
+    location: "Test Location"
+    start: "2023-01-01"
+    tags:
+      - "Test Tag"
+`;
+const exampleEventYaml2 = `
+  - name: "Test Event 2"
+    priority: 7
+    location: "Test Location"
+    start: "2023-01-12"
+    tags:
+      - "Test Tag"
+`;
+const biweeklyEventYaml = `
+  - name: "Biweekly Event"
+    priority: 5
+    start: "2023-01-01"
+    frequency: "biweekly"
+    tags: []
+`;
+const biweeklyEventWithEndTimeYaml = `
+  - name: Kingmaker
+    priority: 8
+    location: Online
+    start: 2023-03-01 18:30:00 PT
+    end: 2023-03-01 21:30:00 PT
+    frequency: biweekly
+    tags:
+      - Pathfinder
+`;
+
+describe('events', () => {
+	test('loads the example data when environment variable is unset', async () => {
+		vi.stubEnv('DATA_FILE', undefined);
+		vol.fromJSON({
+			'./example/data.yaml': `upcoming:${exampleEventYaml}`,
+		});
+
+		const result = await events(DateTime.fromISO('2022-01-01T00:00:00.000-08:00'), 1);
+
+		expect(result).toBeInstanceOf(Array);
+		expect(result.length).toBe(1);
+		expect(result[0].length).toBe(2);
+		expect(result[0][0]).toEqual('Sun Jan 01 2023');
+		expect(result[0][1][0].name).toEqual('Test Event');
+	});
+
+	test('watches the data file', async () => {
+		vi.useFakeTimers();
+		vi.stubEnv('DATA_FILE', 'data.yaml');
+		vol.fromJSON({
+			'./data.yaml': `upcoming:${exampleEventYaml}`,
+		});
+
+		const result = await events(DateTime.fromISO('2022-01-01T00:00:00.000-08:00'), 1);
+		expect(result[0][1][0].name).toEqual('Test Event');
+
+		vol.fromJSON({
+			'./data.yaml': `upcoming:${exampleEventYaml2}`,
+		});
+		vi.advanceTimersByTime(10_000);
+
+		const newResult = await events(DateTime.fromISO('2022-01-01T00:00:00.000-08:00'), 1);
+		expect(newResult[0][1][0].name).toEqual('Test Event 2');
+		vi.useRealTimers();
+	});
+
+	test('handles biweekly events', async () => {
+		vi.stubEnv('DATA_FILE', 'biweekly/data.yaml');
+		vol.fromJSON({
+			'./biweekly/data.yaml': `upcoming:${biweeklyEventYaml}`,
+		});
+
+		const result = await events(DateTime.fromISO('2022-01-01T00:00:00.000-08:00'), 5);
+
+		expect(result).toBeInstanceOf(Array);
+		expect(result.length).toBe(5);
+		expect(result[0][0]).toEqual('Sun Jan 01 2023');
+		expect(result[1][0]).toEqual('Sun Jan 15 2023');
+		expect(result[2][0]).toEqual('Sun Jan 29 2023');
+		expect(result[3][0]).toEqual('Sun Feb 12 2023');
+		expect(result[4][0]).toEqual('Sun Feb 26 2023');
+	});
+
+	test('handles biweekly events with end times', async () => {
+		vi.stubEnv('DATA_FILE', 'biweekly/data.yaml');
+		vol.fromJSON({
+			'./biweekly/data.yaml': `upcoming:${biweeklyEventWithEndTimeYaml}`,
+		});
+
+		const result = await events(DateTime.fromISO('2022-01-01T00:00:00.000-08:00'), 5);
+
+		expect(result).toBeInstanceOf(Array);
+		expect(result.length).toBe(5);
+		expect(result[0][0]).toEqual('Wed Mar 01 2023');
+		expect(result[1][0]).toEqual('Wed Mar 15 2023');
+		expect(result[2][0]).toEqual('Wed Mar 29 2023');
+		expect(result[3][0]).toEqual('Wed Apr 12 2023');
+		expect(result[4][0]).toEqual('Wed Apr 26 2023');
+	});
+});
+
+describe('reset', () => {
+	test('throws an error if not in test environment', () => {
+		vi.stubEnv('NODE_ENV', 'production');
+		expect(() => {
+			reset();
+		}).toThrow();
+	});
+
+	test('does nothing if not initialized', () => {
+		vi.spyOn(console, 'log');
+		reset();
+		expect(console.log).not.toHaveBeenCalled();
+	});
+});

src/lib/util.test.ts
@@ -1,0 +1,32 @@
+import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
+
+import { debounce } from './util';
+
+describe('debounce', () => {
+	beforeEach(() => {
+		vi.useFakeTimers();
+	});
+	afterEach(() => {
+		vi.restoreAllMocks();
+	});
+
+	test('calls callback only when it should', async () => {
+		const callback = vi.fn();
+		const debounced = debounce(callback, 1000);
+
+		debounced();
+		expect(callback).not.toHaveBeenCalled();
+
+		vi.advanceTimersByTime(100);
+		debounced();
+		debounced();
+		expect(callback).not.toHaveBeenCalled();
+
+		vi.advanceTimersByTime(1000);
+		expect(callback).toHaveBeenCalledTimes(1);
+
+		debounced();
+		vi.advanceTimersByTime(1000);
+		expect(callback).toHaveBeenCalledTimes(2);
+	});
+});